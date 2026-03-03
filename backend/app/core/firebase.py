d4e697cdfcb0108fcd93789b2d551e15a7ae9962
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-03-03 15:42:27 -0300

    firebase production env config

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -1,7 +1,16 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-if not firebase_admin._apps:
+firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+
+if firebase_json:
+    cred_dict = json.loads(firebase_json)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+else:
+    # fallback para ambiente local
     cred = credentials.Certificate("firebase_key.json")
     firebase_admin.initialize_app(cred)
 
